68242d86655d476706831514232725e4b428ce06
6320502479 <pattrapol.jam@gmail.com>
2023-02-11 15:19:53 +0700

    Create test.tsx

---
+++ client/src/pages/test.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+//import axios from 'axios';
+//import { Link } from 'react-router-dom';
+import "../style.css";
+
+const Book = () => {
+
+  return (
+    <div>
+      <h1>Doo Book Shop</h1>
+      <div className="">
+            <button className="">Delete</button>
+            <button className="">Update</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Book
