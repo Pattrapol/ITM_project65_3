2bfe70560778c38556830844dd83f518f1069d46
autsadawut <autsadawut.dew10@gmail.com>
2023-02-11 12:36:59 +0700

    start_project
    -setup -> backend, frontend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mysql from 'mysql';
+
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"face_detect"
+})
+
+app.get("/", (req,res)=>{
+    res.json("Hello this is the backend")
+})
+
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend")
+})
