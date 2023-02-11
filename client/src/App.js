b19c828c54fe4a6f5e253a02f3dab8468cb6c123
6320502479 <pattrapol.jam@gmail.com>
2023-02-11 23:15:43 +0700

    v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Book from "../src/pages/test";
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import {Chartimport} from "./Chartim";
 
+const DateInput = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    return (
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    );
+  };
+  
+  const buttonText=<button>hello</button>;
 function App() {
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Book/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+      
+    <Chartimport/>
+    
   );
 }
 
